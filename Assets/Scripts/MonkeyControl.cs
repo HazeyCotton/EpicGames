f2c58cc9e3c7b8e93e4e2ea5f4b44a06e174df6b
Ford Lascari <ford.lascari@gmail.com>
2022-06-29 16:07:17 -0400

    Running Fox in ball and animations

--- Assets/Scripts/MonkeyControl.cs
+++ Assets/Scripts/MonkeyControl.cs
@@ -1,19 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
 
 public class MonkeyControl : MonoBehaviour
 {
     public GameObject player;
     private PlayerController ctrl;
 
+    private Animator anim;
+    private Rigidbody rbody;
+
+    public float animationSpeed = 1f;
+    public float rootMovementSpeed = 1f;
+    public float rootTurnSpeed = 1f;
+
+    float _inputForward = 0f;
+    float _inputTurn = 0f;
+
     void Awake()
     {
         ctrl = player.GetComponent<PlayerController>();
+        if (ctrl == null)
+            Debug.Log("Player controller could not be found");
+
+        anim = GetComponent<Animator>();
+        if (anim == null)
+            Debug.Log("Animator could not be found");
+
     }
 
-    void Update()
+    void OnMove(InputValue movementValue)
     {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        _inputForward = movementVector.y;
+        _inputTurn = movementVector.x;
+        Debug.Log(_inputForward);
+    }
+
+    void FixedUpdate()
+    {
+        
+        anim.SetFloat("velx", _inputTurn);
+        anim.SetFloat("vely", _inputForward);
+
         transform.rotation = Quaternion.Euler(
             ctrl.GetRotation().x / Mathf.PI * 180, 
             ctrl.GetRotation().y / Mathf.PI * 180, 
