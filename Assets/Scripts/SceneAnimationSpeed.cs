1956e11b421b2f367f7fa726eeb1be7d87e9d9bd
Ford Lascari <ford.lascari@gmail.com>
2022-07-11 19:04:48 -0400

    drag and drop

--- Assets/Scripts/SceneAnimationSpeed.cs
+++ Assets/Scripts/SceneAnimationSpeed.cs
@@ -6,12 +6,21 @@ public class SceneAnimationSpeed : MonoBehaviour
 {
     public Animator[] animatorControlledObstacles;
 
+    public GameObject[] movingObstacles;
+
     private float animationSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
         animationSpeed = 1f;
+
+        movingObstacles = GameObject.FindGameObjectsWithTag("ObstacleOrRailing");
+
+        if (movingObstacles.Length == 0)
+        {
+            Debug.Log("No game objects are tagged with ObstacleOrRailing");
+        }
     }
 
     // Update is called once per frame
@@ -23,9 +32,26 @@ public class SceneAnimationSpeed : MonoBehaviour
     public void setAnimationSpeed(float newSpeed)
     {
         animationSpeed = newSpeed;
-        foreach (Animator anim in animatorControlledObstacles)
+        
+        // Current way game is setup has obstacles and railings tagged as the same
+        // Will now go through extra things than just obstacles
+        // Therefore slightly ineefecient but will have good error handling
+        foreach (GameObject obj in movingObstacles)
         {
-            anim.speed = animationSpeed;
+            SimpleObstacleController cont = obj.GetComponent<SimpleObstacleController>();
+            if (cont != null)
+            {
+                cont.setRotationRate(animationSpeed);
+                continue;
+            }
+
+            Animator anim = obj.GetComponent<Animator>();
+            if (anim != null)
+            {
+                anim.speed = animationSpeed;
+                continue;
+            }
+
         }
     }
 }
