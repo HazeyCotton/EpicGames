d125f3e24835042d51aeb946f8efe3e14c21a473
Ford Lascari <ford.lascari@gmail.com>
2022-07-13 13:29:31 -0400

    sphere pretty much finalized

---
+++ Assets/glass/script/Rotate_self.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate_self : MonoBehaviour
+{
+    public float speed;
+    public float x = -1;
+    public float y = -1;
+    public float z = -1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    float num = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        num += Time.deltaTime * this.speed;
+        if (num > 360)
+            num = 0;
+
+
+        if (this.x == -1)
+            this.transform.rotation = Quaternion.Euler(num, this.y, this.z);
+        if (this.y == -1)
+            this.transform.rotation = Quaternion.Euler(this.x, num, this.z);
+        if (this.z == -1)
+            this.transform.rotation = Quaternion.Euler(this.x, this.y, num);
+    }
+}
