9a4e2562a27cee29cc1115af8eedee0a3f5da9f3
Ford Lascari <ford.lascari@gmail.com>
2022-07-16 07:45:05 -0400

    pre playtestingcommit

--- Assets/Scripts/spinningSquare.cs
+++ Assets/Scripts/spinningSquare.cs
@@ -4,15 +4,63 @@ using UnityEngine;
 
 public class spinningSquare : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private bool clockwise;
+
+    private bool rotating;
+
+    private float rotationRate;
+
+    private float relativeRotation;
+
     void Start()
     {
-        
+        clockwise = true;
+        rotating = false;
+        rotationRate = 4f;
+        relativeRotation = 0f;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (rotating)
+        {
+            if (clockwise)
+            {
+                rotateSquare(-rotationRate);
+            } else {
+                rotateSquare(rotationRate);
+            }
+        }
     }
+
+    private void rotateSquare(float rotValue)
+    {
+        float nextZ = transform.localEulerAngles.z + rotValue;
+        relativeRotation += rotValue;
+        if (clockwise) {
+            if (relativeRotation <= -360f)
+            {
+                
+                clockwise = !clockwise;
+                rotating = false;
+            } 
+        } else {
+            if (relativeRotation >= 360f)
+            {
+                clockwise = !clockwise;
+                rotating = false;
+            }
+        } 
+        transform.localEulerAngles = new Vector3(
+                transform.localEulerAngles.x,
+                transform.localEulerAngles.y,
+                nextZ);
+    }
+
+    public void startRotating()
+    {
+        rotating = true;
+    }
+
 }
