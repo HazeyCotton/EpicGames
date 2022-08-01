42525dd8354b6519ddaa09d761ccb31b33c65182
CHazey <chaelton3@gatech.edu>
2022-08-01 07:14:19 -0400

    volume changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -54,7 +54,7 @@ public class PlayerController : MonoBehaviour
          Debug.Log(this.transform.rotation.y);
          Debug.Log(5f * Mathf.Cos(this.transform.rotation.y/180f*Mathf.PI));
          Debug.Log(5f * Mathf.Sin(this.transform.rotation.y/180f*Mathf.PI));*/
-        engineVolumeMinimum = 0.3f;
+        engineVolumeMinimum = 0.4f;
 
         accelerationCounter = 0;
 
